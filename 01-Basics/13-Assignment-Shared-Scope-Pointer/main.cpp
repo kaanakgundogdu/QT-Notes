fd63d54eca827200ec81011a0ec0027e456496d4
Kaan Akgundogdu <kaanakgundogdu@gmail.com>
2022-11-10 14:47:42 +0300

    13-Assignment-Shared-Scope-Pointer/ added.

--- 01-Basics/13-Assignment-Shared-Scope-Pointer/main.cpp
+++ 01-Basics/13-Assignment-Shared-Scope-Pointer/main.cpp
@@ -1,8 +1,23 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include <QScopedPointer>
+#include <QSharedPointer>
+#include "test.h"
+
+Test* get_object(){
+    return new Test();
+}
+
+void test(){
+    QSharedPointer<Test> obj_1 (get_object());
+    obj_1->setObjectName("Obj 1");
+    QScopedPointer<Test> obj_2 (get_object());
+    obj_2->setObjectName("Obj 2");
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    test();
     return a.exec();
 }
